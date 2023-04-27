76b76b9b38304df3fba9b791eae8ed4b05fd27d5
Nick <nickphilomath@gmail.com>
2023-04-27 16:40:11 -0400

    completed tutorial 12

--- Vulkan/main.cpp
+++ Vulkan/main.cpp
@@ -1,18 +1,20 @@
-#include "Application.hpp"
+#include "first_app.hpp"
+
+// std
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
 
 int main() {
-	lve::Application app{};
+    lve::FirstApp app{};
 
-	try {
-		app.run();
-	}
-	catch (const std::exception& e) {
-		std::cerr << e.what() << '\n';
-		return EXIT_FAILURE;
-	}
+    try {
+        app.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        return EXIT_FAILURE;
+    }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
